bb73abb2f9d2878377d4bacfe72c4571a9923945
Helmut Kemper <helmut.kemper@gmail.com>
2021-11-30 13:35:12 -0300

    initial commit

--- mainProject/typeServer.go
+++ mainProject/typeServer.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/hashicorp/memberlist"
+	"github.com/helmutkemper/iotmaker.docker.builder.demo/grpcProto"
 	"github.com/helmutkemper/util"
 	"google.golang.org/grpc"
 	"io/ioutil"
 	"log"
 	"net"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -25,9 +28,11 @@ const (
 )
 
 type Server struct {
+	grpcProto.UnimplementedSyncInstancesServer
+
 	thisInstanceIsReady        bool
 	thisNodeAddress            string
-	syncPort                   string
+	syncPort                   int
 	serviceNameList            []string
 	memberList                 *memberlist.Memberlist
 	syncBetweenInstancesTicker *time.Ticker
@@ -71,7 +76,7 @@ func (e *Server) getAndUpdateThisInstanceAddress() (IP string, ready bool) {
 	return
 }
 
-func (e *Server) Init(syncPort string, config *memberlist.Config, servicesListNames ...string) (err error) {
+func (e *Server) Init(syncPort int, config *memberlist.Config, servicesListNames ...string) (err error) {
 	var ipAddress string
 
 	e.syncPort = syncPort
@@ -121,6 +126,8 @@ func (e *Server) Init(syncPort string, config *memberlist.Config, servicesListNa
 				}
 
 				e.thisNodeAddress = ipAddress
+
+				e.dataTransmission()
 			}
 		}
 	}(e)
@@ -174,12 +181,166 @@ func (e *Server) serverGrpc(ip string) (err error) {
 
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
-	grpcProto.RegisterSyncCacheDaraServerServer(grpcServer, e)
+	grpcProto.RegisterSyncInstancesServer(grpcServer, e)
 	err = grpcServer.Serve(lis)
 	if err != nil {
 		log.Printf("serverGrpc().grpcServer.Serve().error: %v", err)
 		return
 	}
+
+	return
+}
+
+func (e *Server) dataTransmission() {
+	var err error
+	var ipListToSync []string
+	var nodeShutdownList map[string]string
+	var nodeAddedList map[string]string
+	ipListToSync, nodeShutdownList, nodeAddedList = e.getMembersListToSyncDataAndManagerSyncList()
+
+	var conn *grpc.ClientConn
+	var client grpcProto.SyncInstancesClient
+
+	for _, ipAddress := range nodeAddedList {
+		log.Printf("ip address %v has been added", ipAddress)
+	}
+
+	for _, ipAddress := range nodeShutdownList {
+		log.Printf("ip address %v is shunted down", ipAddress)
+	}
+
+	for _, podIpAddress := range ipListToSync {
+		conn, client, err = e.clientGrpcOpen(podIpAddress + ":" + strconv.Itoa(e.syncPort))
+		if err != nil {
+			log.Printf("e.clientGrpcOpen().error: %v", err)
+			return
+		}
+
+		var ctx context.Context
+		var cancelContext context.CancelFunc
+		var ready *grpcProto.InstanceIsReadyReplay
+		ctx, cancelContext = context.WithTimeout(context.Background(), 500*time.Millisecond)
+		ready, err = client.GrpcFuncInstanceIsReady(ctx, &grpcProto.Empty{})
+		cancelContext()
+		if err != nil {
+			e.clientGrpcClose(conn)
+			log.Printf("client.GrpcFuncInstanceIsReady().error: %v", err)
+			continue
+		}
+
+		if ready.GetIsReady() == false {
+			continue
+		}
+
+		ctx, cancelContext = context.WithTimeout(context.Background(), 500*time.Millisecond)
+		_, err = client.GrpcFuncCommunication(ctx, &grpcProto.Empty{})
+		cancelContext()
+		if err != nil {
+			e.clientGrpcClose(conn)
+			log.Printf("client.GrpcFuncCommunication().error: %v", err)
+			continue
+		}
+	}
+
+	return
+}
+
+func (e *Server) clientGrpcOpen(serverAddr string) (conn *grpc.ClientConn, client grpcProto.SyncInstancesClient, err error) {
+	conn, err = grpc.Dial(serverAddr, grpc.WithInsecure())
+	if err != nil {
+		log.Printf("grpc.Dial(%v).error: %v", serverAddr, err)
+		return
+	}
+
+	client = grpcProto.NewSyncInstancesClient(conn)
+	return
+}
+
+func (e *Server) getMembersListToSyncDataAndManagerSyncList() (ipList []string, nodeShutdownList, nodeAddedList map[string]string) {
+	//lista de nodes atual
+	ipList = make([]string, 0)
+
+	// pega a lista de nodes desligados
+	// chave: nome do node / valor: ip do node
+	nodeShutdownList = make(map[string]string)
+
+	// pega a lista de nodes adicionados
+	// chave: nome do node / valor: ip do node
+	nodeAddedList = make(map[string]string)
+
+	if e.nodeNamesList == nil {
+		e.nodeNamesList = new(sync.Map)
+	}
+
+	var ipAddress string
+
+	// testa se um node tem nome registrado e um ip diferente do original
+	var originalIpAddress interface{}
+
+	// preenche a lista atual de membros
+	// chave: nome do node / valor: ip do node
+	var nodeActualMembersList = make(map[string]string)
+	for _, node := range e.memberList.Members() {
+		ipAddress = e.ipAddressClear(node.Address())
+		nodeActualMembersList[node.Name] = ipAddress
+
+		ipList = append(ipList, ipAddress)
+	}
+
+	// pega a lista de nodes desligados
+	// chave: nome do node / valor: ip do node
+	var found bool
+	e.nodeNamesList.Range(func(nodeName, nodeIpAddress interface{}) (continueLoop bool) {
+		_, found = nodeActualMembersList[nodeName.(string)]
+		if found == false {
+			nodeShutdownList[nodeName.(string)] = nodeIpAddress.(string)
+		}
+
+		continueLoop = true
+		return
+	})
+
+	// pega a lista de nodes adicionados
+	// chave: nome do node / valor: ip do node
+	for nodeName, nodeIpAddress := range nodeActualMembersList {
+		originalIpAddress, found = e.nodeNamesList.Load(nodeName)
+		if found == false {
+			nodeAddedList[nodeName] = nodeIpAddress
+		}
+
+		// adiciona o node a lista permanente de nodes
+		e.nodeNamesList.Store(nodeName, nodeIpAddress)
+
+		// procura um bug ou falha de conceito
+		if originalIpAddress != nil && originalIpAddress.(string) != nodeActualMembersList[nodeName] {
+			log.Printf("bug: o endereço IP do node está variando para o node.name")
+		}
+	}
+
+	return
+}
+
+func (e *Server) clientGrpcClose(conn *grpc.ClientConn) {
+	var err error
+	err = conn.Close()
+	if err != nil {
+		log.Printf("clientGrpcClose().error: %v", err)
+		return
+	}
+
+	return
+}
+
+func (e *Server) grpcFuncInstanceIsReady(context.Context, *grpcProto.Empty) (replay *grpcProto.InstanceIsReadyReplay, err error) {
+	replay = &grpcProto.InstanceIsReadyReplay{
+		IsReady: e.thisInstanceIsReady,
+	}
+
+	return
+}
+
+func (e *Server) GrpcFuncCommunication(context.Context, *grpcProto.Empty) (empty *grpcProto.Empty, err error) {
+	empty = &grpcProto.Empty{}
 
 	return
 }
